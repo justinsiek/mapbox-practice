87e37acbb5174e877bafcbb782476d9f55368820
justinsiek <justinsiek05@gmail.com>
2025-05-29 17:05:40 -0700

    feat: added block group outlines

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,7 +4,7 @@ import { useRef, useEffect } from 'react'
 import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-mapboxgl.accessToken =  process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
 
 const Main = () => {
   const mapContainer = useRef<HTMLDivElement | null>(null);
@@ -15,9 +15,28 @@ const Main = () => {
     
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v12', 
       center: [-117.82410, 33.68525],
-      zoom: 11.55
+      zoom: 9
+    });
+
+    map.current.on('load', () => {map.current?.addSource('blockgroup-boundaries', {
+        'type': 'geojson',
+        'data': '/orange_county_blockgroups.geojson'
+      });
+
+      map.current?.addLayer({
+        'id': 'blockgroup-outlines',
+        'type': 'line',
+        'source': 'blockgroup-boundaries',
+        'layout': {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        'paint': {
+          'line-color': '#555',
+          'line-width': 0.6
+        }
+      });
     });
 
     return () => {
